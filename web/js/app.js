27cb2c4d78dcbe701aaa76927622791d8d21d919
EricMoz <ericmoczydlowski@yahoo.com>
2026-06-13 22:43:21 -0400

    batch 1/3 + 2/3 + 3/3: Theatre Lights Up video sizing (pin .film-theatre-player min-width:1080px + width:100% so box is stable at full size independent of Up Next title length; upnext already capped + title wraps/ellipsises). Filter responsive collapse at 950px. Find your daCATs hover to match other buttons. Site build 20260614-7

--- web/js/app.js
+++ web/js/app.js
@@ -1021,7 +1021,7 @@ function scrollToCollectorCollection(opts) {
   }
 }
 
-var browseAdvancedMq = window.matchMedia("(max-width: 850px)");
+var browseAdvancedMq = window.matchMedia("(max-width: 950px)");
 
 /** Collapse filter/sort on mobile (archive + portfolio); always open on desktop archive. */
 function syncBrowseAdvancedPanel() {
