9bd6a0fc629c299dbea3e54f2fc7d3ce5020b613
EricMoz <ericmoczydlowski@yahoo.com>
2026-07-15 21:54:13 -0400

    chore(site): bump deploy build to 20260716-1
    Cache-bust CSS/JS/HTML and service worker so film↔NFT link updates
    ship to universe.dacat.fun after Pages deploy.

--- web/sw.js
+++ web/sw.js
@@ -9,7 +9,7 @@
  *
  * CACHE name is bumped by scripts/bump_deploy_version.py each deploy.
  */
-const CACHE = "dacat-gallery-v20260713-12";
+const CACHE = "dacat-gallery-v20260716-1";
 const SHELL = [
   "./manifest.webmanifest",
   "./assets/brand/dacat-icon-64.png",
